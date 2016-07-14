2a0d4aad9ebc2e36a8a3305d762e38af2c7e5143
Eduardo Martin Rojo <emr@mib105656i.local>
2016-07-14 12:56:44 +0100

    Improving performance for displaying operations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,9 @@ require 'date'
 
 class Activity < ActiveRecord::Base
   validates :activity_type, :presence => true
+  validates :instrument, :presence => true
+  validates :kit, :presence => true
+  validates :asset_group, :presence => true
   belongs_to :activity_type
   belongs_to :instrument
   belongs_to :kit
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -3,4 +3,6 @@ class Operation < ApplicationRecord
   belongs_to :asset
   belongs_to :action
   belongs_to :activity
+
+  scope :for_presenting, ->() { includes(:asset, :action) }
 end
