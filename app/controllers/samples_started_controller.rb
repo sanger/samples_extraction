69c5527bab1e6c761f3ee1b9dbccc686ba6b90e9
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2016-08-04 17:09:09 +0100

    Added pagination to Started samples

--- app/controllers/samples_started_controller.rb
+++ app/controllers/samples_started_controller.rb
@@ -1,5 +1,27 @@
 class SamplesStartedController < ApplicationController
   def index
     @activity_types = ActivityType.all.visible
+
+    @activities_for_activity_types = @activity_types.map do |activity_type|
+      {
+        :activity_type => activity_type,
+        :activities => activity_type.activities.paginate(pagination_params_for_activity_type(activity_type))
+      }
+    end
+    @activity_type_selected = ActivityType.find_by_id(samples_started_params[:activity_type_id])
+  end
+
+  private
+
+  def pagination_params_for_activity_type(activity_type)
+    if samples_started_params[:activity_type_id].to_i==activity_type.id
+      {:page => samples_started_params[:page], :per_page => 5}
+    else
+      {:page => 1, :per_page => 5}
+    end
+  end
+
+  def samples_started_params
+    params.permit(:activity_type_id, :page)
   end
 end
