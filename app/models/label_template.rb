2a0ec7d3e1477e3ae74964395d69a26544cbf534
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-02-17 00:01:40 +0000

    Changed plate template

--- app/models/label_template.rb
+++ app/models/label_template.rb
@@ -8,6 +8,8 @@ class LabelTemplate < ActiveRecord::Base
       'Tube' => ['Tube', 'SampleTube']
     }.select{|k,v| v.include?(type)}.first[0]
 
-    where(:template_type => type)
+    barcodetype = 'ean13' # This needs to be specified by fact
+    templates = where(:template_type => type)
+    [templates.select{|t| t.name.include?(barcodetype)}].flatten.first || templates.first
   end
 end
