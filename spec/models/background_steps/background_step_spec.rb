0fd2f2e7c835b481749c78fd55e31782706493ff
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-10-15 08:13:49 -0400

    Added test to check that it does not execute steps if one of the connected list has failed

--- spec/models/background_steps/background_step_spec.rb
+++ spec/models/background_steps/background_step_spec.rb
@@ -18,18 +18,30 @@ RSpec.describe 'BackgroundStep' do
       end
 
       context 'when a background step is completed' do
-        context 'when it has a next step configured' do
+        context 'when there was an error' do
           let(:another_step) { build :background_step }
-          it 'executes the next step' do
+
+          it 'does not execute the next step' do
             step.update_attributes(next_step: another_step)
             allow(another_step).to receive(:execute_actions)
-            step.update_attributes(state: 'complete')
-            expect(another_step).to have_received(:execute_actions)
+            step.update_attributes(state: 'error')
+            expect(another_step).not_to have_received(:execute_actions)
           end
         end
-        context 'when it does not have a next step' do
-          it 'does not execute the next step' do
-            expect{step.update_attributes(state: 'complete')}.not_to raise_error
+        context 'when there was no error in its execution' do
+          context 'when it has a next step configured' do
+            let(:another_step) { build :background_step }
+            it 'executes the next step' do
+              step.update_attributes(next_step: another_step)
+              allow(another_step).to receive(:execute_actions)
+              step.update_attributes(state: 'complete')
+              expect(another_step).to have_received(:execute_actions)
+            end
+          end
+          context 'when it does not have a next step' do
+            it 'does not execute the next step' do
+              expect{step.update_attributes(state: 'complete')}.not_to raise_error
+            end
           end
         end
       end
