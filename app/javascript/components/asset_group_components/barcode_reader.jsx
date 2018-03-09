58a9f8fd234df82dd51c12a2efafdeab35b239c7
Eduardo Martin Rojo <emr@sanger.ac.uk>
2018-03-09 17:04:05 +0000

    In progress

--- app/javascript/components/asset_group_components/barcode_reader.jsx
+++ app/javascript/components/asset_group_components/barcode_reader.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 
 class BarcodeReader extends React.Component {
+  readBarcodes(barcodesStr) {
+    return barcodesStr.split(' ').map(function(val) {
+      return val.replace(/\"\'/, '');
+    });
+  }
+
+  readInput(e) {
+    if ((e.keyCode === 9) || (e.keyCode == 13)) {
+      this.readBarcodes(e)
+    }
+  }
+
+  onReadBarcodes(e) {
+    this.readInput(e)
+  }
+
   render() {
     return(
       <div>
@@ -10,7 +26,8 @@ class BarcodeReader extends React.Component {
             id="asset_group_delete_barcode" value="" />
           <input type="hidden" name="asset_group[delete_all_barcodes]"
             id="asset_group_delete_all_barcodes" value="false" />
-          <input name='asset_group[add_barcode]' id="asset_group_add_barcode" className="form-control" type='text' placeholder='Scan a barcode' />
+          <input name='asset_group[add_barcode]' id="asset_group_add_barcode" className="form-control" type='text' placeholder='Scan a barcode' 
+            onKeyDown={this.onReadBarcodes} />
           <span className="input-group-btn">
             <button className="btn btn-default barcode-send" title="Send barcode">
               <span className="glyphicon glyphicon-arrow-down"></span>
