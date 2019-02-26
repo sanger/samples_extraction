cf0f99fcd103467b1b6a7280e73cc5af08de8b24
Eduardo Martin Rojo <emr@sanger.ac.uk>
2019-02-26 09:44:31 +0000

    Set up label template for code39 with the same template sequencescap uses

--- script/plate_to_code39.rb
+++ script/plate_to_code39.rb
@@ -1,4 +1,10 @@
-lt = LabelTemplate.find_by(name: 'se_ean13_96plate')
-lt.update_attributes(template_type: 'DEPRECATED_PLATE')
+ActiveRecord::Base.transaction do
+  pmb_template_name = "sqsc_96plate_label_template_code39"
+  lt = LabelTemplate.find_by(name: 'se_ean13_96plate')
+  lt.update_attributes(template_type: 'DEPRECATED_PLATE')
 
-LabelTemplate.create(name: 'sqsc_96plate_label_template_code39', template_type: 'Plate', external_id: 198)
+  external_id = PMB::LabelTemplate.where(name: pmb_template_name).first.id
+
+  LabelTemplate.create(name: pmb_template_name,
+    template_type: 'Plate', external_id: external_id)
+end
