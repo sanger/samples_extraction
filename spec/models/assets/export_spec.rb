801e3930fcbf66f78580ef4f50bdc0e8a2e955a6
Eduardo Martin Rojo <emr@sanger.ac.uk>
2019-05-16 23:25:58 +0100

    Relocating test not in the right folder. Removed file not in use anymore

--- spec/models/assets/export_spec.rb
+++ spec/models/assets/export_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Asset, type: :model do
+RSpec.describe 'Asset::Export' do
   describe 'Export' do
     context '#racking_info' do
       it 'generates an attribute object for a well' do
@@ -26,7 +26,7 @@ RSpec.describe Asset, type: :model do
                    :contains          :tube1, :tube2, :tube3, :tube4 .
         }
         @assets = SupportN3::parse_facts(facts)
-        
+
         @rack1 = Asset.find_by(uuid: 'rack1')
         expect(@rack1.attributes_to_update).to eq([
           {sample_tube_uuid: "s1", location: "A1"},
@@ -75,7 +75,7 @@ RSpec.describe Asset, type: :model do
           :s3 :a :SampleTube .
           :s4 :a :SampleTube .
           :rack2   :a                 :TubeRack ;
-                   :contains          :tube1, :tube2, :tube3, :tube4 .          
+                   :contains          :tube1, :tube2, :tube3, :tube4 .
           :tube1   :a                 :Tube ;
                    :location          "A1" ;
                    :sample_tube       :s1 .
