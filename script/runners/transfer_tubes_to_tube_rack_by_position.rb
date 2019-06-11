d826ad0d3246df2b39aded1a6d2b8e0bff6b5f68
Eduardo Martin Rojo <emr@sanger.ac.uk>
2019-06-11 23:38:53 +0100

    Added test for transfer tubes to tube rack by position

--- script/runners/transfer_tubes_to_tube_rack_by_position.rb
+++ script/runners/transfer_tubes_to_tube_rack_by_position.rb
@@ -33,16 +33,60 @@ class TransferTubesToTubeRackByPosition
     ((location[1..-1].to_i - 1) * max_row)+ (location[0].ord - 'A'.ord);
   end
 
+  def wells_for(rack)
+    wells_ids = rack.facts.with_predicate('contains').map(&:object_asset_id)
+    Asset.where(id: wells_ids)
+  end
+
+  def validate_enough_tubes_for_all_wells(tubes, wells, updates)
+    if tubes.length > wells.length
+      updates.set_errors(["This rack does not have enough spaces to allocate #{tubes.length} tubes"])
+      return false
+    end
+    return true
+  end
+
+  def validate_tube_are_not_in_rack(rack, tubes, updates)
+    facts_for_wells = Fact.where(asset_id: wells_for(rack).map(&:id))
+    previous_tubes_ids = facts_for_wells.with_predicate('transferredFrom').map(&:object_asset_id)
+    tubes_already_ids = previous_tubes_ids & tubes.map(&:id)
+    if (tubes_already_ids.length > 0)
+      tubes_already = Asset.where(id: tubes_already_ids).map(&:barcode)
+      updates.set_errors(["This rack already contains the tubes #{tubes_already.to_s}"])
+      return false
+    end
+    true
+  end
+
+  def validate_all_tubes_have_aliquot(tubes, updates)
+    aliquot_facts = Fact.where(asset_id: tubes.map(&:id), predicate: 'aliquotType')
+    unless aliquot_facts.size == tubes.size
+      updates.set_errors(["Not all tubes have an aliquot associated"])
+      return false
+    end
+    unless (aliquot_facts.map(&:object).uniq.size == 1)
+      updates.set_errors(["Not all tubes have the same aliquot"])
+      return false
+    end
+    true
+  end
+
   def process
     FactChanges.new.tap do |updates|
       aliquot_types = []
       if assets_compatible_with_step_type
         tubes = asset_group.assets.joins(:facts).where(facts: {predicate: 'transferToTubeRackByPosition'}).uniq
         rack = tubes.first.facts.with_predicate('transferToTubeRackByPosition').first.object_asset
+
+        return updates unless validate_tube_are_not_in_rack(rack, tubes, updates)
+
         wells = rack.facts.with_predicate('contains').map(&:object_asset).sort_by do |elem|
           location = elem.facts.with_predicate('location').first.object
           location_to_pos(location)
         end.reject{|w| w.has_predicate?('transferredFrom')}.uniq
+
+        return updates unless validate_enough_tubes_for_all_wells(tubes, wells, updates)
+
         asset_group.assets.with_predicate('transferToTubeRackByPosition').zip(wells).each do |asset, well|
           if asset && well
             asset.facts.with_predicate('aliquotType').each do |f_aliquot|
@@ -56,6 +100,9 @@ class TransferTubesToTubeRackByPosition
           end
         end
         if aliquot_types
+
+          return updates unless validate_all_tubes_have_aliquot(tubes, updates)
+
           if (((aliquot_types.uniq.length) == 1) && (aliquot_types.uniq.first == 'DNA'))
             purpose_name = 'DNA Stock Plate'
           elsif (((aliquot_types.uniq.length) == 1) && (aliquot_types.uniq.first == 'RNA'))
