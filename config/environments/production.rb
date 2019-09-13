bd1fa5f86c70df6006cee52005ff7afac1434954
Eduardo Martin Rojo <emr@sanger.ac.uk>
2019-09-13 13:44:59 +0100

    Default production env to be able to generate assets without failing initializers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,82 +1,72 @@
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = false
+  config.webpacker.check_yarn_integrity = true
 
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = true
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
+  config.action_cable.allowed_request_origins = [%r{https?://\S+}]
+
+  # Do not eager load code on boot.
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
+  # Show full error reports and disable caching.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like
-  # NGINX, varnish or squid.
-  # config.action_dispatch.rack_cache = true
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :notify
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+  config.allow_concurrency = true
+  config.redis_enabled = true
 
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  # Use a different logger for distributed setups
+  # config.logger = SyslogLogger.new
+  config.log_level = :info
+  config.log_tags = [:request_id]
+  config.time_zone = 'London'
+  config.eager_load = true
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = :debug
 
-  # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 
-  # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.pmb_uri = ENV.fetch('PMB_URI', '')
+  config.ss_uri =  ENV.fetch('SS_URI', '')
+  config.ss_api_v2_uri = ENV.fetch('SS_API_V2_URI', '')
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.ss_authorisation = ENV.fetch('SS_AUTHORISATION_TOKEN', '')
+  config.searcher_name_by_barcode = 'Find assets by barcode'
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.searcher_name_by_barcode = 'Find assets by barcode'
+  config.printing_enabled = ENV.fetch('PRINTING_ENABLED', '')=='true'
+  config.printing_disabled = !config.printing_enabled
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.inference_engine = :default
+  config.cwm_path = ENV.fetch('CWM_PATH', '')
+  config.default_n3_resources_url = ENV.fetch('N3_RESOURCES_URL', '')
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  config.enable_reasoning = true
 
-  # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
-
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
 end
