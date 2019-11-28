cdc2a156c9159c823cc872b996ae932989ead0c9
Katy Taylor <kt17@sanger.ac.uk>
2019-11-28 09:58:41 +0000

    forgot to commit this line

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_130805) do
+ActiveRecord::Schema.define(version: 20191127154249) do
 
   create_table "actions", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "action_type", null: false
