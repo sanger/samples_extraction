9d2d91db54000b704a8077ea4c574e95905236a0
James Glover <james.glover@sanger.ac.uk>
2020-11-04 08:16:12 +0000

    Fix Dockerfile for new ruby version
    The previous Dockerfile was using an unofficial ruby + node base
    image. However, this was locked at ruby 2.5.
    
    This switches us to the official Ruby image, and installs node manually.

--- config/initializers/warren.rb
+++ config/initializers/warren.rb
@@ -2,4 +2,8 @@
 
 require 'warren'
 
-Warren.setup(Rails.application.config.warren.deep_symbolize_keys.slice(:type, :config))
+if Rails.application.config.warren.present?
+  Warren.setup(Rails.application.config.warren.deep_symbolize_keys.slice(:type, :config))
+else
+  Rails.logger.warn "Warren.yml not configured for #{ENV['RAILS_ENV']}"
+end
