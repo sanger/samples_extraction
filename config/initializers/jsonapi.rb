2bb2baecdc5e7776d638322ce3d9b4f6285d2df1
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-02-02 11:22:19 +0000

    Remove config to use the defaults

--- config/initializers/jsonapi.rb
+++ config/initializers/jsonapi.rb
@@ -3,9 +3,6 @@ JSONAPI.configure do |config|
   config.json_key_format = :underscored_key
   config.resource_key_type = :uuid
 
-  # optional request features
-  config.default_allow_include_to_many = true
-
   config.default_page_size = 25
   config.maximum_page_size = 1000
 end
